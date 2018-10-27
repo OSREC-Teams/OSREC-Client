62ba469be0b0ce5a7159f102e394849766e8ced8
Nicolas Godfrin <n.godfrin@gmail.com>
2018-10-27 18:29:53 +0200

    Add header for landing page and set proType

--- src/pages/LandingPage/Header.js
+++ src/pages/LandingPage/Header.js
@@ -1,1 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const Header = ({ children }) => <div>{children}</div>;
+
+Header.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default Header;

5807708a371b6d3ca6f1712889d0b06f6173c9e3
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-11-10 16:58:34 +0100

    Setup base of redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+
+import getStore from 'store';
+
 import App from 'pages/App';
 import 'index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={getStore()}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);

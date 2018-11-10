5a682bd1737bbfeb633fd5c6d41c3b360ad861ea
Thibault Miranda de Oliveira <thibault@mirandadeoliveira.com>
2018-11-10 20:46:52 +0100

    Add switch between ProtectedRouter and GuestRouter

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import LandingPage from 'pages/LandingPage';
 
-const App = () => (
+const ProtectedRouter = () => <Router />;
+
+const GuestRouter = () => (
   <Router>
     <Route path="/" component={LandingPage} />
   </Router>
 );
 
-export default App;
+const App = ({ loggedIn }) => (
+  <React.Fragment>
+    {loggedIn ? <ProtectedRouter /> : <GuestRouter />}
+  </React.Fragment>
+);
+
+App.propTypes = {
+  loggedIn: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = () => ({
+  loggedIn: false,
+});
+
+export default connect(mapStateToProps)(App);
--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function getStore(initialState) {
   return createStore(
-    {},
+    () => {},
     initialState,
     composeEnhancers(applyMiddleware(thunk)),
   );
